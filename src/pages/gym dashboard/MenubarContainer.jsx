25b771ddf9ed1f0012998b874fd9a46833f8d76b
GopalNavetia <work.gopalnavetia@gmail.com>
2025-10-31 16:14:25 +0530

    Page Container Routes Added

--- src/pages/gym dashboard/MenubarContainer.jsx
+++ src/pages/gym dashboard/MenubarContainer.jsx
@@ -2,6 +2,7 @@ import './MenubarContainer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faUser, faDumbbell, faCoins } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 export default function MenubarContainer() {
 
@@ -13,13 +14,21 @@ export default function MenubarContainer() {
         setIsVisible((prev) => !prev);
     };
 
+    const navigate = useNavigate();
+
+    // Member Button
+    function handelMemberButtonClick() {
+        navigate('/gymdashboard');
+    }
+
+
     return (
         <div className="menubar" style={{ width: isExpanded ? "14%" : "3.5%" }}>
             <div className="menuItems">
                 <span className='barIcon' onClick={toggleMenubar}><FontAwesomeIcon icon={faBars} /></span>
                 <hr />
                 <div className='container'>
-                    <div className='items'><span className='icons'><FontAwesomeIcon icon={faUser} /></span><h3 style={{ display: isVisible ? "" : "none" }}>Members</h3></div>
+                    <div className='items' onClick={handelMemberButtonClick}><span className='icons'><FontAwesomeIcon icon={faUser} /></span><h3 style={{ display: isVisible ? "" : "none" }}>Members</h3></div>
                     <div className='items'><span className='icons'><FontAwesomeIcon icon={faDumbbell} /></span><h3 style={{ display: isVisible ? "" : "none" }}>Trainers</h3></div>
                     <div className='items'><span className='icons'><FontAwesomeIcon icon={faCoins} /></span><h3 style={{ display: isVisible ? "" : "none" }}>Finance</h3></div>
                 </div>
--- src/pages/gym dashboard/PageContainer.jsx
+++ src/pages/gym dashboard/PageContainer.jsx
@@ -1,10 +1,17 @@
 import './PageContainer.css'
 import MemberPage from '../member page/MemberPage';
+import { useRoutes } from 'react-router-dom';
+
+const GymDashboardPageRoutes = () => {
+    return useRoutes([
+        { path: '/', element: <MemberPage /> },
+    ]);
+};
 
 export default function PageContainer() {
     return (
         <div className='PageContainer'>
-            <MemberPage />
+            <GymDashboardPageRoutes />
         </div>
     );
 }
