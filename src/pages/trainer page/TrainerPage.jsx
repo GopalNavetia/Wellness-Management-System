98d7e7cfea9252ae54fa025983a5e92b2bdfeaa2
GopalNavetia <work.gopalnavetia@gmail.com>
2025-12-06 12:44:10 +0530

    Trainer Card API + Owner's Backend URL Set

--- src/pages/trainer page/TrainerPage.jsx
+++ src/pages/trainer page/TrainerPage.jsx
@@ -4,35 +4,34 @@ import TrainerTable from './TrainerTable'
 import AddTrainer from '../trainer page/AddTrainer'
 import TrainerProfile from './TrainerProfile'
 import { useRoutes } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axiosInstance from '../../utils/AxiosInstance'
 
 export default function TrainerPage() {
 
     // ----- API CODE -----
-    // const [fetchData, setFetchData] = useState(null);
-    // const [loading, setLoading] = useState(true);
-    // const [selectedMemberId, setSelectedMemberId] = useState(null); // useState, not let
+    const [fetchData, setFetchData] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    // useEffect(() => {
-    //     async function fetchMemberCardData() {
-    //         try {
-    //             const response = await axiosInstance.get("/MyProject/MemberCardDetail", {
-    //                 headers: { "ngrok-skip-browser-warning": "true" }
-    //             });
-    //             setFetchData(response.data);
-    //         } catch (error) {
-    //             console.error('Error fetching member data:', error);
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     }
-    //     fetchMemberCardData();
-    // }, []);
+    useEffect(() => {
+        async function fetchTrainerCardData() {
+            try {
+                const response = await axiosInstance.get("/MyProject/TrainerCardDetailAPI", {
+                    headers: { "ngrok-skip-browser-warning": "true" }
+                });
+                setFetchData(response.data);
+            } catch (error) {
+                console.error('Error fetching member data:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchTrainerCardData();
+    }, []);
 
-    // if (loading) {
-    //     return <p style={{ textAlign: 'center' }}>Loading...</p>;
-    // }
+    if (loading) {
+        return <p style={{ textAlign: 'center' }}>Loading...</p>;
+    }
 
     // Trainer Page Routes
     const routes = useRoutes([
@@ -41,13 +40,13 @@ export default function TrainerPage() {
             element: <>
                 <div className="infoCardContainer">
 
-                    {/* <Card title='Total Trainers' data={fetchData.total_trainers} />
+                    <Card title='Total Trainers' data={fetchData.total_trainers} />
                     <Card title='Active Trainers' data={fetchData.active_trainers} />
-                    <Card title='Inactive Trainers' data={fetchData.inactive_trainers} /> */}
+                    <Card title='Inactive Trainers' data={fetchData.inactive_trainers} />
 
-                    <Card title='Total Trainers' data='5' />
+                    {/* <Card title='Total Trainers' data='5' />
                     <Card title='Active Trainers' data='4' />
-                    <Card title='Inactive Trainers' data='1' />
+                    <Card title='Inactive Trainers' data='1' /> */}
                 </div>
                 <TrainerTable />
             </>
--- src/utils/BackendURL.jsx
+++ src/utils/BackendURL.jsx
@@ -1,2 +1,3 @@
-let BackendURL = "https://admonitorial-cinderella-hungerly.ngrok-free.dev"
+// let BackendURL = "https://admonitorial-cinderella-hungerly.ngrok-free.dev"
+let BackendURL = "https://stephan-unprying-yvonne.ngrok-free.dev"
 export default BackendURL
