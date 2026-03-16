99afe1498f812aebda82305f8e0c46c65e27e402
GopalNavetia <work.gopalnavetia@gmail.com>
2026-03-16 12:33:06 +0530

    Member Profile + ACtion Button Responsive

--- src/utils/BackendURL.jsx
+++ src/utils/BackendURL.jsx
@@ -1,5 +1,6 @@
 // Vite will inject this value from environment variables
 let BackendURL = import.meta.env.VITE_BACKEND_URL;
+// let BackendURL = "https://preconcurrent-kami-unbewildered.ngrok-free.dev";
 // let BackendURL = "https://hueless-wamblingly-sheba.ngrok-free.dev";
 
 export default BackendURL;
