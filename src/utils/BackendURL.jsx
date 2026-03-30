1a3ec71e769bbe21bcee64b0f5497f1951d22097
GopalNavetia <work.gopalnavetia@gmail.com>
2026-03-30 15:39:27 +0530

    Backend URL set

--- src/utils/BackendURL.jsx
+++ src/utils/BackendURL.jsx
@@ -1,7 +1,7 @@
 // Vite will inject this value from environment variables
-// let BackendURL = import.meta.env.VITE_BACKEND_URL;
+let BackendURL = import.meta.env.VITE_BACKEND_URL;
 // let BackendURL = "https://admonitorial-cinderella-hungerly.ngrok-free.dev";
 // let BackendURL = "https://preconcurrent-kami-unbewildered.ngrok-free.dev";
-let BackendURL = "https://hueless-wamblingly-sheba.ngrok-free.dev";
+// let BackendURL = "https://hueless-wamblingly-sheba.ngrok-free.dev";
 
 export default BackendURL;
